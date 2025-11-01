e9767c6ae33aa15a2af9417c47d07a92e0afe511
GIl <cynthia.osorio@live.lagcc.cuny.edu>
2025-11-01 00:58:26 -0400

    add svg assets

---
+++ src/lib/components/icons/vercel.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { ...rest } = $props();
+</script>
+
+<svg
+	viewBox="0 0 256 222"
+	{...rest}
+	width="256"
+	height="222"
+	xmlns="http://www.w3.org/2000/svg"
+	preserveAspectRatio="xMidYMid"><path fill="#000" d="m128 0 128 221.705H0z" /></svg
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import ShadCN from './../lib/components/icons/shadCN.svelte';
 	import Bun from '$lib/components/icons/Bun.svelte';
 	import Javascript from '$lib/components/icons/Javascript.svelte';
 	import React from '$lib/components/icons/React.svelte';
@@ -7,7 +8,7 @@
 	import Aws from '$lib/components/icons/Aws.svelte';
 	import Postgres from '$lib/components/icons/Postgres.svelte';
 	import Redis from '$lib/components/icons/Redis.svelte';
-	import Css from '$lib/components/icons/Css.svelte';
+
 	import Typescript from '$lib/components/icons/Typescript.svelte';
 	import Sqlite from '$lib/components/icons/Sqlite.svelte';
 	import Tailwind from '$lib/components/icons/Tailwind.svelte';
@@ -17,10 +18,17 @@
 	import Expo from '$lib/components/icons/Expo.svelte';
 	import Convex from '$lib/components/icons/Convex.svelte';
 	import Tanstack from '$lib/components/icons/Tanstack.svelte';
+	import Vercel from '$lib/components/icons/vercel.svelte';
+	import Linux from '$lib/components/icons/linux.svelte';
+	import Sql from '$lib/components/icons/Sql.svelte';
+	import Git from '$lib/components/icons/git.svelte';
+	import Github from '$lib/components/icons/github.svelte';
+	import Sst from '$lib/components/icons/sst.svelte';
+	import shadcn from '$lib/components/icons/shadCN.svelte';
 </script>
 
 <h1>Skills</h1>
-<div class="flex h-screen items-center justify-center">
+<div class=" flex h-screen items-center justify-center">
 	<Bun class="w-1/2" /> <br /> <span>Bun</span>
 	<Javascript class="w-1/2" /> <span>JavaScript</span> <br />
 	<React class="w-1/2" /> <span>React</span> <br />
@@ -29,7 +37,6 @@
 	<Aws class="w-1/2" />
 	<Postgres class="w-1/2" /> <span>PostgreSQL</span> <br />
 	<Redis class="w-1/2" /> <span>Redis</span> <br />
-	<Css class="w-1/2" />
 	<Typescript class="w-1/2" /> <span>TypeScript</span> <br />
 	<Sqlite class="w-1/2" /> <span>SQLite</span> <br />
 	<Tailwind class="w-1/2" /> <span>Tailwind</span> <br />
@@ -39,4 +46,11 @@
 	<Expo class="w-1/2" /> <span>Expo</span> <br />
 	<Convex class="w-1/2" /> <span>Convex</span> <br />
 	<Tanstack class="w-1/2" /> <span>Tanstack</span> <br />
+	<Vercel class="w-1/2" /> <span>Vercel</span> <br />
+	<Linux class="w-1/2" /> <span>Linux</span> <br />
+	<Sql class="w-1/2" /> <span>SQL</span> <br />
+	<Git class="w-1/2" /> <span>Git</span> <br />
+	<Github class="w-1/2" /> <span>GitHub</span> <br />
+	<Sst class="w-1/2" /> <span>SST</span> <br />
+	<ShadCN class="w-1/2" /> <span>Shadcn</span> <br />
 </div>
