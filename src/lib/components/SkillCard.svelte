10ad30bb87da17d48026fbc554f8afa2fb5ae089
GIl <cynthia.osorio@live.lagcc.cuny.edu>
2025-11-01 15:18:08 -0400

    wiring up comp comp for some fun

---
+++ src/lib/components/SkillCard.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { skill } = $props();
+</script>
+
+<div class="flex flex-col items-center justify-center bg-foreground p-4">
+	<h3 class="text-2xl font-bold text-background">{skill}</h3>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import SkillCard from '$lib/components/SkillCard.svelte';
 </script>
 
 <h1 class="text-4xl font-bold">Skills</h1>
+
+<SkillCard skill="JavaScript" />
+<SkillCard skill="TypeScript" />
+<SkillCard skill="Svelte" />
+<SkillCard skill="React" />
+<SkillCard skill="Next.js" />
+<SkillCard skill="Tailwind CSS" />
+<SkillCard skill="HTML" />
+<SkillCard skill="CSS" />
+<SkillCard skill="Git" />
+<SkillCard skill="GitHub" />
+<SkillCard skill="GitLab" />
