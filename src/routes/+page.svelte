92baa236947e27261c3891dbe3f39ff08f945041
GIl <cynthia.osorio@live.lagcc.cuny.edu>
2025-11-01 00:11:48 -0400

    add svg assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Bun from '$lib/components/icons/Bun.svelte';
+	import Javascript from '$lib/components/icons/Javascript.svelte';
+	import React from '$lib/components/icons/React.svelte';
+	import Svelte from '$lib/components/icons/Svelte.svelte';
+	import Python from '$lib/components/icons/Python.svelte';
+	import Aws from '$lib/components/icons/Aws.svelte';
+	import Postgres from '$lib/components/icons/Postgres.svelte';
+	import Redis from '$lib/components/icons/Redis.svelte';
+	import Css from '$lib/components/icons/Css.svelte';
+	import Typescript from '$lib/components/icons/Typescript.svelte';
+	import Sqlite from '$lib/components/icons/Sqlite.svelte';
+	import Tailwind from '$lib/components/icons/Tailwind.svelte';
+	import Node from '$lib/components/icons/Node.svelte';
+	import Vite from '$lib/components/icons/Vite.svelte';
+	import Docker from '$lib/components/icons/Docker.svelte';
+	import Expo from '$lib/components/icons/Expo.svelte';
+	import Convex from '$lib/components/icons/Convex.svelte';
+	import Tanstack from '$lib/components/icons/Tanstack.svelte';
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<Bun class="w-1/2" /> <br /> <span>Bun</span>
+	<Javascript class="w-1/2" /> <span>JavaScript</span> <br />
+	<React class="w-1/2" /> <span>React</span> <br />
+	<Svelte class="w-1/2" /> <span>Svelte</span> <br />
+	<Python class="w-1/2" /> <span>Python</span> <br />
+	<Aws class="w-1/2" />
+	<Postgres class="w-1/2" /> <span>PostgreSQL</span> <br />
+	<Redis class="w-1/2" /> <span>Redis</span> <br />
+	<Css class="w-1/2" />
+	<Typescript class="w-1/2" /> <span>TypeScript</span> <br />
+	<Sqlite class="w-1/2" /> <span>SQLite</span> <br />
+	<Tailwind class="w-1/2" /> <span>Tailwind</span> <br />
+	<Node class="w-1/2" /> <span>Node</span> <br />
+	<Vite class="w-1/2" /> <span>Vite</span> <br /> <br />
+	<Docker class="w-1/2" /> <span>Docker</span> <br />
+	<Expo class="w-1/2" /> <span>Expo</span> <br />
+	<Convex class="w-1/2" /> <span>Convex</span> <br />
+	<Tanstack class="w-1/2" /> <span>Tanstack</span> <br />
+</div>
